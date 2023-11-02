852f0d558c7aeda9e5c157cb52872620b9687341
Dilip Rathod <rathod.dt@gmail.com>
2023-11-02 19:09:23 +0530

    Updated startup script - command to install qLIKsENSE

--- basic-commands/startup-script.ps1
+++ basic-commands/startup-script.ps1
@@ -2,3 +2,6 @@ New-Item -Path 'c:\build' -ItemType Directory
  #New-Item -Path 'c:\build\test.txt' -ItemType File
 gcloud --version
 gsutil rsync -r "gs://my-poc-dilip/" "C:\build"
+
+$insatll_status=Start-Process `
+        -FilePath C:\build\Qlik_Sense_Desktop_feb_2023_setup.exe 
